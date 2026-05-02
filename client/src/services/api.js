8cda38ab0f32d609afd20daca3b7c122de9b712d
Nipun Nirmal Samarathunga <141556293+nipunnirmal21@users.noreply.github.com>
2026-05-02 23:01:49 +0530

    yoli

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://yoli-backend.onrender.com/api' });
 
 export const fetchProducts = (params = {}) =>
     api.get('/products', { params }).then((r) => r.data?.products ?? []);
